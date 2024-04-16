11a53a635928f2971e3059d53dcbe547aa537692
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-16 17:57:04 +0530

    String

---
+++ String/all.js
@@ -0,0 +1,58 @@
+// var $ans1 = "asd";
+// console.log($ans1);
+// ######################
+
+//    - Useful string methods.
+var str1 = "Royal technosoft P ltd.";
+// console.log(str1);
+// console.log(str1[1]);
+
+// 1) str1.length
+// console.log(str1.length);
+// console.log(str1[str1.length-1]);
+
+// 2) str1.replace()
+// console.log(str1.replace(`tech`,'Tech'));
+
+// 3) str1.replaceAll()
+// console.log(str1.replaceAll(`tech`,'Tech'));
+
+// 4)  str1.charAt(index)
+    // console.log(str1.charAt(0));
+    
+//   5)  str1.charCodeAt()
+    // A = 65
+    // console.log(str1.charCodeAt(0));
+
+// 6) str1.concat();
+// var ans = str1.concat(" Royal");
+// console.log(ans);
+
+                    //pass :   char/string
+// console.log(str1.includes("tech"));
+
+// checking starting point
+// console.log(str1.indexOf("e"));
+
+// split   : return Array 
+// console.log(str1.split('t'));
+
+// str1.trimStart
+// console.log(str1.trimStart());
+// console.log(str1.trimEnd());
+// console.log(str1.trim());
+
+// str1.toUpperCase
+// str1.toLowerCase
+
+// // slice 
+        //     starting , ending index     remember : always starting index < ending index
+console.log(str1.slice(10,10));
+// str1.substring
+        // starting and ending index     remember : higher index always 
+console.log(str1.substring(10,10));
+// substr 
+        //  index  : length 
+console.log(str1.substr(10,10));
+
+
