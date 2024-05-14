361f2f0f6297b45e572069a3da13fcd6d7cc8f2a
Shabhaizafar <zafarShabhai03@gmail.com>
2024-05-14 17:51:37 +0530

    Array

--- Array Methods/ArrayMethod3.js
+++ Array Methods/ArrayMethod3.js
@@ -60,3 +60,48 @@
 // Arr.map
 // Arr.reduce
 // Arr.splice
+
+///////////////////////////////////
+
+// Tasks : 
+// Fill Method
+// Basic Fill Usage
+// 1_ Create an array with 5 elements and fill it with the value 0.
+
+///-----------------------------------------------
+// Partial Fill
+//2)  Create an array [1, 2, 3, 4, 5] and fill the values from index 1 to 3 with the value 7.
+
+///-----------------------------------------------
+// Fill with Dynamic Values
+//3)  Create an array of length 10 and fill it with the value of the current index (i.e., [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).
+
+///-----------------------------------------------
+// Filter Method
+// Basic Filter Usage
+
+//4)  Filter the even numbers from an array [1, 2, 3, 4, 5, 6].
+
+///-----------------------------------------------
+// Filter by String Length
+
+//5)  Filter an array of strings ['apple', 'banana', 'cherry', 'date'] to include only those strings with more than 5 characters.
+
+///-----------------------------------------------
+// Filter with Objects
+//6)  Given an array of objects {name: string, age: number}, filter out those objects where age is less than 18.
+
+///-----------------------------------------------
+// Find Method
+// Basic Find Usage
+
+//7) Find the first even number in an array [1, 3, 7, 8, 10].
+
+///-----------------------------------------------
+// Find Object in Array
+//8)  Given an array of objects {name: string, age: number}, find the object with the name 'Jane'.
+
+///-----------------------------------------------
+// Find Index Using Find
+//9) Find the index of the first number greater than 10 in the array [2, 5, 8, 12, 15].
+///-----------------------------------------------
